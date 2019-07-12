bef160bc01d807946ed2224285490479ca56869c
Lucas Vasconcelos <lucasv1992@gmail.com>
2019-07-12 00:24:28 -0300

    criada classe que é responsavel por criar o ambiente para os testes de integração e criado startup de testes

---
+++ tests/ERP.Tests.Integration/Environment.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Services.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net.Http;
+
+namespace ERP.Tests.Integration
+{
+    public class Environment
+    {
+        public static TestServer Server;
+        public static HttpClient Client;
+
+        public static void CreateServer()
+        {
+            Server = new TestServer(new WebHostBuilder().UseEnvironment("Test").UseUrls("http://localhost:99899").UseStartup<StartupTests>());
+            Client = Server.CreateClient();
+        }
+    }
+}
