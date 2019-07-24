5c6fb15f7d29d60e1b26e06843970f0725c6229a
Lucas Vasconcelos <lucasv1992@gmail.com>
2019-07-24 01:50:00 -0300

    testes integração usuário

---
+++ tests/ERP.Tests.Integration/Gerencial/DTO/UsuarioDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ERP.Tests.Integration.Gerencial.DTO
+{
+    public class UsuarioDTO
+    {
+        public bool success { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public string access_token { get; set; }
+        public DateTime expires_in { get; set; }
+        public User user { get; set; }
+    }
+
+    public class User
+    {
+        public string id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public Claim[] claims { get; set; }
+    }
+
+    public class Claim
+    {
+        public string type { get; set; }
+        public string value { get; set; }
+    }
+
+}
