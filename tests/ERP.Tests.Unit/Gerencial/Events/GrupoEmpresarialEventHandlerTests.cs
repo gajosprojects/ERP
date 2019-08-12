8019a899661015ffea2905b732a7761b2ed36af2
Lucas Vasconcelos <lucasv1992@gmail.com>
2019-08-12 01:07:25 -0300

    finalizando testes de comandos e eventos e criando factories para entidades e comandos

--- tests/ERP.Tests.Unit/Gerencial/Events/GrupoEmpresarialEventHandlerTests.cs
+++ tests/ERP.Tests.Unit/Gerencial/Events/GrupoEmpresarialEventHandlerTests.cs
@@ -1,4 +1,7 @@
 ﻿using ERP.Gerencial.Domain.GruposEmpresariais.Events;
+using ERP.Gerencial.Domain.GruposEmpresariais.Events.Cnaes;
+using ERP.Gerencial.Domain.GruposEmpresariais.Events.Empresas;
+using ERP.Gerencial.Domain.GruposEmpresariais.Events.Estabelecimentos;
 using ERP.Gerencial.Domain.GruposEmpresariais.Events.GruposEmpresariais;
 using System;
 using System.Threading;
@@ -34,6 +37,69 @@ namespace ERP.Tests.Unit.Gerencial.Events
         public void GrupoEmpresarialEventHandler_DeletedGrupoEmpresarialEvent_RetornarSucesso()
         {
             DeletedGrupoEmpresarialEvent notification = new DeletedGrupoEmpresarialEvent(Guid.NewGuid(), Guid.NewGuid());
+            Assert.Equal(_grupoEmpresarialEventHandler.Handle(notification, new CancellationToken()), Task.CompletedTask);
+        }
+
+        [Fact]
+        public void GrupoEmpresarialEventHandler_SavedCnaeEvent_RetornarSucesso()
+        {
+            SavedCnaeEvent notification = new SavedCnaeEvent(Guid.NewGuid(), true, Guid.NewGuid(), DateTime.Now, DateTime.Now, "Cnae1", "Cnae1", Guid.NewGuid());
+            Assert.Equal(_grupoEmpresarialEventHandler.Handle(notification, new CancellationToken()), Task.CompletedTask);
+        }
+
+        [Fact]
+        public void GrupoEmpresarialEventHandler_UpdatedCnaeEvent_RetornarSucesso()
+        {
+            UpdatedCnaeEvent notification = new UpdatedCnaeEvent(Guid.NewGuid(), true, Guid.NewGuid(), DateTime.Now, DateTime.Now, "Cnae1", "Cnae1", Guid.NewGuid());
+            Assert.Equal(_grupoEmpresarialEventHandler.Handle(notification, new CancellationToken()), Task.CompletedTask);
+        }
+
+        [Fact]
+        public void GrupoEmpresarialEventHandler_DeletedCnaeEvent_RetornarSucesso()
+        {
+            DeletedCnaeEvent notification = new DeletedCnaeEvent(Guid.NewGuid(), Guid.NewGuid());
+            Assert.Equal(_grupoEmpresarialEventHandler.Handle(notification, new CancellationToken()), Task.CompletedTask);
+        }
+
+        [Fact]
+        public void GrupoEmpresarialEventHandler_SavedEmpresaEvent_RetornarSucesso()
+        {
+            SavedEmpresaEvent notification = new SavedEmpresaEvent(Guid.NewGuid(), true, Guid.NewGuid(), DateTime.Now, DateTime.Now, "Empresa1", "Empresa1", "Empresa1", "empresa@gmail.com", "empresa.com.br", false, DateTime.Now, null, "", "", 1, Guid.NewGuid());
+            Assert.Equal(_grupoEmpresarialEventHandler.Handle(notification, new CancellationToken()), Task.CompletedTask);
+        }
+
+        [Fact]
+        public void GrupoEmpresarialEventHandler_UpdatedEmpresaEvent_RetornarSucesso()
+        {
+            UpdatedEmpresaEvent notification = new UpdatedEmpresaEvent(Guid.NewGuid(), true, Guid.NewGuid(), DateTime.Now, DateTime.Now, "Empresa1", "Empresa1", "Empresa1", "empresa@gmail.com", "empresa.com.br", false, DateTime.Now, null, "", "", 1, Guid.NewGuid());
+            Assert.Equal(_grupoEmpresarialEventHandler.Handle(notification, new CancellationToken()), Task.CompletedTask);
+        }
+
+        [Fact]
+        public void GrupoEmpresarialEventHandler_DeletedEmpresaEvent_RetornarSucesso()
+        {
+            DeletedEmpresaEvent notification = new DeletedEmpresaEvent(Guid.NewGuid(), Guid.NewGuid());
+            Assert.Equal(_grupoEmpresarialEventHandler.Handle(notification, new CancellationToken()), Task.CompletedTask);
+        }
+
+        [Fact]
+        public void GrupoEmpresarialEventHandler_SavedEstabelecimentoEvent_RetornarSucesso()
+        {
+            SavedEstabelecimentoEvent notification = new SavedEstabelecimentoEvent(Guid.NewGuid(), true, Guid.NewGuid(), DateTime.Now, DateTime.Now, "Estabelecimento1", "Estabelecimento1", "Estabelecimento1", "0123456789", "0123456789", "estabelecimento@gmail.com", "estabelecimento.com.br", false, DateTime.Now, null, true, "", "", 1, Guid.NewGuid(), Guid.NewGuid());
+            Assert.Equal(_grupoEmpresarialEventHandler.Handle(notification, new CancellationToken()), Task.CompletedTask);
+        }
+
+        [Fact]
+        public void GrupoEmpresarialEventHandler_UpdatedEstabelecimentoEvent_RetornarSucesso()
+        {
+            UpdatedEstabelecimentoEvent notification = new UpdatedEstabelecimentoEvent(Guid.NewGuid(), true, Guid.NewGuid(), DateTime.Now, DateTime.Now, "Estabelecimento1", "Estabelecimento1", "Estabelecimento1", "0123456789", "0123456789", "estabelecimento@gmail.com", "estabelecimento.com.br", false, DateTime.Now, null, true, "", "", 1, Guid.NewGuid(), Guid.NewGuid());
+            Assert.Equal(_grupoEmpresarialEventHandler.Handle(notification, new CancellationToken()), Task.CompletedTask);
+        }
+
+        [Fact]
+        public void GrupoEmpresarialEventHandler_DeletedEstabelecimentoEvent_RetornarSucesso()
+        {
+            DeletedEstabelecimentoEvent notification = new DeletedEstabelecimentoEvent(Guid.NewGuid(), Guid.NewGuid());
             Assert.Equal(_grupoEmpresarialEventHandler.Handle(notification, new CancellationToken()), Task.CompletedTask);
         }
     }
---
+++ tests/ERP.Tests.Unit/Gerencial/Factories/CommandFactory.cs
@@ -0,0 +1,305 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using ERP.Gerencial.Domain.GruposEmpresariais.Commands.Cnaes;
+using ERP.Gerencial.Domain.GruposEmpresariais.Commands.Empresas;
+using ERP.Gerencial.Domain.GruposEmpresariais.Commands.Estabelecimentos;
+using ERP.Gerencial.Domain.GruposEmpresariais.Commands.GruposEmpresariais;
+using System;
+
+namespace ERP.Tests.Unit.Gerencial.Factories
+{
+    public static class CommandFactory
+    {
+        public static SaveGrupoEmpresarialCommand GenerateValidSaveGrupoEmpresarialCommand()
+        {
+            return new Faker<SaveGrupoEmpresarialCommand>("pt_BR").CustomInstantiator(c => new SaveGrupoEmpresarialCommand(
+                c.Company.CompanyName(), 
+                c.Company.CompanyName(), 
+                Guid.NewGuid()))
+            .Generate();
+        }
+
+        public static SaveGrupoEmpresarialCommand GenerateInvalidSaveGrupoEmpresarialCommand()
+        {
+            return new Faker<SaveGrupoEmpresarialCommand>("pt_BR") .CustomInstantiator(c => new SaveGrupoEmpresarialCommand(
+                (Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString()),
+                c.Company.CompanyName(),
+                Guid.NewGuid()))
+            .Generate();
+        }
+
+        public static UpdateGrupoEmpresarialCommand GenerateValidUpdateGrupoEmpresarialCommand()
+        {
+            return new Faker<UpdateGrupoEmpresarialCommand>("pt_BR").CustomInstantiator(c => new UpdateGrupoEmpresarialCommand(
+                Guid.Parse("04681edc-c9fe-4a39-97e7-c02582f5cde1"),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a")))
+            .Generate();
+        }
+
+        public static UpdateGrupoEmpresarialCommand GenerateInvalidUpdateGrupoEmpresarialCommand()
+        {
+            return new Faker<UpdateGrupoEmpresarialCommand>("pt_BR").CustomInstantiator(c => new UpdateGrupoEmpresarialCommand(
+                Guid.NewGuid(),
+                (Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString()),
+                c.Company.CompanyName(),
+                Guid.NewGuid()))
+            .Generate();
+        }
+
+        public static DeleteGrupoEmpresarialCommand GenerateValidDeleteGrupoEmpresarialCommand()
+        {
+            return new DeleteGrupoEmpresarialCommand(Guid.Parse("04681edc-c9fe-4a39-97e7-c02582f5cde1"), Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"));
+        }
+
+        public static DeleteGrupoEmpresarialCommand GenerateInvalidDeleteGrupoEmpresarialCommand()
+        {
+            return new DeleteGrupoEmpresarialCommand(Guid.NewGuid(), Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"));
+        }
+
+        public static SaveCnaeCommand GenerateValidSaveCnaeCommand()
+        {
+            return new Faker<SaveCnaeCommand>("pt_BR").CustomInstantiator(c => new SaveCnaeCommand(
+                c.Company.CompanyName().Substring(0, 4),
+                c.Company.CompanyName(),
+                Guid.NewGuid(),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a")))
+            .Generate();
+        }
+
+        public static SaveCnaeCommand GenerateInvalidSaveCnaeCommand()
+        {
+            return new Faker<SaveCnaeCommand>("pt_BR").CustomInstantiator(c => new SaveCnaeCommand(
+                (Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString()),
+                c.Company.CompanyName(),
+                Guid.NewGuid(),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a")))
+            .Generate();
+        }
+
+        public static UpdateCnaeCommand GenerateValidUpdateCnaeCommand()
+        {
+            return new Faker<UpdateCnaeCommand>("pt_BR").CustomInstantiator(c => new UpdateCnaeCommand(
+                Guid.Parse("04681edc-c9fe-4a39-97e7-c02582f5cde1"),
+                c.Company.CompanyName().Substring(0, 4),
+                c.Company.CompanyName(),
+                Guid.NewGuid(),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a")))
+            .Generate();
+        }
+
+        public static UpdateCnaeCommand GenerateInvalidUpdateCnaeCommand()
+        {
+            return new Faker<UpdateCnaeCommand>("pt_BR").CustomInstantiator(c => new UpdateCnaeCommand(
+                Guid.NewGuid(),
+                (Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString()),
+                c.Company.CompanyName(),
+                Guid.NewGuid(),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a")))
+            .Generate();
+        }
+
+        public static DeleteCnaeCommand GenerateValidDeleteCnaeCommand()
+        {
+            return new DeleteCnaeCommand(Guid.Parse("04681edc-c9fe-4a39-97e7-c02582f5cde1"), Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"));
+        }
+
+        public static DeleteCnaeCommand GenerateInvalidDeleteCnaeCommand()
+        {
+            return new DeleteCnaeCommand(Guid.NewGuid(), Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"));
+        }
+
+        public static SaveEmpresaCommand GenerateValidSaveEmpresaCommand()
+        {
+            return new Faker<SaveEmpresaCommand>("pt_BR").CustomInstantiator(c => new SaveEmpresaCommand(
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"),
+                c.Company.CompanyName().Substring(0, 4),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                c.Person.Email,
+                c.Person.Website,
+                false,
+                DateTime.Now,
+                null,
+                c.Company.CatchPhrase(),
+                c.Company.Cnpj().Replace(".", "").Replace("-", "").Replace("/", ""),
+                1,
+                Guid.NewGuid()))
+            .Generate();
+        }
+
+        public static SaveEmpresaCommand GenerateInvalidSaveEmpresaCommand()
+        {
+            return new Faker<SaveEmpresaCommand>("pt_BR").CustomInstantiator(c => new SaveEmpresaCommand(
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"),
+                (Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString()),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                c.Person.Email,
+                c.Person.Website,
+                false,
+                DateTime.Now,
+                null,
+                c.Company.CatchPhrase(),
+                c.Company.Cnpj().Replace(".", "").Replace("-", "").Replace("/", ""),
+                1,
+                Guid.NewGuid()))
+            .Generate();
+        }
+
+        public static UpdateEmpresaCommand GenerateValidUpdateEmpresaCommand()
+        {
+            return new Faker<UpdateEmpresaCommand>("pt_BR").CustomInstantiator(c => new UpdateEmpresaCommand(
+                Guid.Parse("04681edc-c9fe-4a39-97e7-c02582f5cde1"),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"),
+                c.Company.CompanyName().Substring(0, 4),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                c.Person.Email,
+                c.Person.Website,
+                false,
+                DateTime.Now,
+                null,
+                c.Company.CatchPhrase(),
+                c.Company.Cnpj().Replace(".", "").Replace("-", "").Replace("/", ""),
+                1,
+                Guid.NewGuid()))
+            .Generate();
+        }
+
+        public static UpdateEmpresaCommand GenerateInvalidUpdateEmpresaCommand()
+        {
+            return new Faker<UpdateEmpresaCommand>("pt_BR").CustomInstantiator(c => new UpdateEmpresaCommand(
+                Guid.Parse("04681edc-c9fe-4a39-97e7-c02582f5cde1"),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"),
+                (Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString()),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                c.Person.Email,
+                c.Person.Website,
+                false,
+                DateTime.Now,
+                null,
+                c.Company.CatchPhrase(),
+                c.Company.Cnpj().Replace(".", "").Replace("-", "").Replace("/", ""),
+                1,
+                Guid.NewGuid()))
+            .Generate();
+        }
+
+        public static DeleteEmpresaCommand GenerateValidDeleteEmpresaCommand()
+        {
+            return new DeleteEmpresaCommand(Guid.Parse("04681edc-c9fe-4a39-97e7-c02582f5cde1"), Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"));
+        }
+
+        public static DeleteEmpresaCommand GenerateInvalidDeleteEmpresaCommand()
+        {
+            return new DeleteEmpresaCommand(Guid.NewGuid(), Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"));
+        }
+
+        public static SaveEstabelecimentoCommand GenerateValidSaveEstabelecimentoCommand()
+        {
+            return new Faker<SaveEstabelecimentoCommand>("pt_BR").CustomInstantiator(c => new SaveEstabelecimentoCommand(
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                "01234567890123456789",
+                "01234567890123456789",
+                c.Person.Email,
+                c.Person.Website,
+                false,
+                DateTime.Now,
+                null,
+                true,
+                c.Company.CatchPhrase(),
+                c.Company.Cnpj().Replace(".", "").Replace("-", "").Replace("/", ""),
+                1,
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde82a"),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde18a")))
+            .Generate();
+        }
+
+        public static SaveEstabelecimentoCommand GenerateInvalidSaveEstabelecimentoCommand()
+        {
+            return new Faker<SaveEstabelecimentoCommand>("pt_BR").CustomInstantiator(c => new SaveEstabelecimentoCommand(
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"),
+                (Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString()),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                "01234567890123456789",
+                "01234567890123456789",
+                c.Person.Email,
+                c.Person.Website,
+                false,
+                DateTime.Now,
+                null,
+                true,
+                c.Company.CatchPhrase(),
+                c.Company.Cnpj().Replace(".", "").Replace("-", "").Replace("/", ""),
+                1,
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde82a"),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde18a")))
+            .Generate();
+        }
+
+        public static UpdateEstabelecimentoCommand GenerateValidUpdateEstabelecimentoCommand()
+        {
+            return new Faker<UpdateEstabelecimentoCommand>("pt_BR").CustomInstantiator(c => new UpdateEstabelecimentoCommand(
+                Guid.Parse("04681edc-c9fe-4a39-97e7-c02582f5cde1"),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                "01234567890123456789",
+                "01234567890123456789",
+                c.Person.Email,
+                c.Person.Website,
+                false,
+                DateTime.Now,
+                null,
+                true,
+                c.Company.CatchPhrase(),
+                c.Company.Cnpj().Replace(".", "").Replace("-", "").Replace("/", ""),
+                1,
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde82a"),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde18a")))
+            .Generate();
+        }
+
+        public static UpdateEstabelecimentoCommand GenerateInvalidUpdateEstabelecimentoCommand()
+        {
+            return new Faker<UpdateEstabelecimentoCommand>("pt_BR").CustomInstantiator(c => new UpdateEstabelecimentoCommand(
+                Guid.NewGuid(), 
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"),
+                (Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString() + Guid.NewGuid().ToString()),
+                c.Company.CompanyName(),
+                c.Company.CompanyName(),
+                "01234567890123456789",
+                "01234567890123456789",
+                c.Person.Email,
+                c.Person.Website,
+                false,
+                DateTime.Now,
+                null,
+                true,
+                c.Company.CatchPhrase(),
+                c.Company.Cnpj().Replace(".", "").Replace("-", "").Replace("/", ""),
+                1,
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde82a"),
+                Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde18a")))
+            .Generate();
+        }
+
+        public static DeleteEstabelecimentoCommand GenerateValidDeleteEstabelecimentoCommand()
+        {
+            return new DeleteEstabelecimentoCommand(Guid.Parse("04681edc-c9fe-4a39-97e7-c02582f5cde1"), Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"));
+        }
+
+        public static DeleteEstabelecimentoCommand GenerateInvalidDeleteEstabelecimentoCommand()
+        {
+            return new DeleteEstabelecimentoCommand(Guid.NewGuid(), Guid.Parse("e7261a1f-18e8-4de6-9b4b-a659a8fde81a"));
+        }
+    }
+}
