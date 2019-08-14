f7bf3a1e9c9c19ff21b965ff35c9ea085d805292
Lucas Vasconcelos <lucasv1992@gmail.com>
2019-08-14 00:08:28 -0300

    adicionada migração que cria check constraint para o tipoIdentificacao de empresa e estabelecimento

--- src/ERP.Infra.Data/Migrations/GruposEmpresariais/20190814024148_CheckConstraintTipoIdentificacao.Designer.cs
+++ src/ERP.Infra.Data/Migrations/GruposEmpresariais/20190814024148_CheckConstraintTipoIdentificacao.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERP.Infra.Data.Migrations.GruposEmpresariais
 {
     [DbContext(typeof(GruposEmpresariaisContext))]
-    partial class GruposEmpresariaisContextModelSnapshot : ModelSnapshot
+    [Migration("20190814024148_CheckConstraintTipoIdentificacao")]
+    partial class CheckConstraintTipoIdentificacao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
