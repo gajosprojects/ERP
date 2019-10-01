ee11bb290592bf7d15625421884e584301677969
Lucas Vasconcelos <lucasv1992@gmail.com>
2019-09-30 23:07:48 -0300

    Recoloca migrações de check_constraint nas tabelas estabelecimento e empresa

---
+++ src/ERP.Infra.Data/Migrations/GruposEmpresariais/20191001012436_CheckConstraintTipoIdentificacao.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERP.Infra.Data.Migrations.GruposEmpresariais
+{
+    public partial class CheckConstraintTipoIdentificacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE empresas ADD CONSTRAINT empresa_check_tipo_identificacao CHECK (tipo_identificacao = 1 OR tipo_identificacao = 2)");
+            migrationBuilder.Sql("ALTER TABLE estabelecimentos ADD CONSTRAINT estabelecimento_check_tipo_identificacao CHECK(tipo_identificacao = 1 OR tipo_identificacao = 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE empresas DROP CONSTRAINT empresa_check_tipo_identificacao;");
+            migrationBuilder.Sql("ALTER TABLE estabelecimentos DROP CONSTRAINT estabelecimento_check_tipo_identificacao;");
+        }
+    }
+}
